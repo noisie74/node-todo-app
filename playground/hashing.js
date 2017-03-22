c752af1b11603bf0ffad5ce37ea25b03d2a18631
Michael B <michaelbor2015@gmail.com>
2017-03-21 22:36:53 -0700

    Hash user

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 4
-};
-var token = jwt.sign(data, '123abd');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abd');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//
+//   });
+// });
+
+var hashedPassword = '$2a$10$m1s1X0ulUqOiMPIdWGm9hudp/sJv754kbPZ4DjtsR.SAbcvojkzja';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 4
+// };
+// var token = jwt.sign(data, '123abd');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abd');
+// console.log('decoded', decoded);
 
 
 // var message = 'I am user number 1';
